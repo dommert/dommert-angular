bcbb73040735496b3b5bbc88975f37600463d9e4
Dommert <jessie.dommert@picmonic.com>
2015-11-24 23:02:52 -0700

    edits

--- dommert-examples/ui-router/friends.js
+++ dommert-examples/ui-router/friends.js
@@ -3,7 +3,7 @@ angular
   .factory('Friends', ['$http', function($http) {
     return {
       get: function() {
-        return $http.get('/friends.json').then(function(response) {
+        return $http.get('#/friends.json').then(function(response) {
           return response.data;
         });
       }
